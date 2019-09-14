f9ef5dc4e76cb17358324b827392a5e139fb794a
Jhohan Silva <jhohanf.silva@gmail.com>
2019-09-14 11:02:47 -0500

    Buscador por artista

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,29 @@
   <div class="hello">
     <h1>Deezer API</h1>
     <h3>Canciones por artista</h3>
-    <ul>
-      <li v-for="(item,index) in items" :key="index">
+    <div class="busqueda">
+      <label>Artista:</label>
+      <input type="text" v-model="artista" />
+      <button v-on:click="filterData()">Buscar</button>
+    </div>
+    <ul v-loading=true>
+      <li v-for="(item,index) in items.slice(0, cantidad)" :key="index">
         <div class="img-preview" style="margin-right: 10px;">
-          <img :src="item.artist.picture_small" />
+          <img :src="item.album.cover_small" />
         </div>
         <div class="descrip">
-          {{ item.title }}
+          <b>{{ item.title }}</b>
           <br />
-          Duración: {{ item.duration }}
+          <span>Duración: {{ item.duration.toString().toHHMMSS() }}</span>
           <br />
+          <span>Albúm: {{ item.album.title }}</span>
+
+          <br />
+          <audio :src="item.preview" preload="none" controls></audio>
         </div>
       </li>
     </ul>
+    <button v-on:click="mostrarMas()">Mostrar más...</button>
   </div>
 </template>
 
@@ -24,23 +34,67 @@ import axios from "axios";
 export default {
   name: "consultaCanciones",
   created() {
-    axios
-      .get("https://deezerdevs-deezer.p.rapidapi.com/search?q=David Guetta", {
-        headers: {
-          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-          "x-rapidapi-key": "0582757109msh175aa52bbe104a7p104799jsn7e282369ac23"
-        }
-      })
-      .then(response => {
-        this.items = response.data.data;
-        console.log(this.items);
-      });
+    // axios
+    //   .get(
+    //     `https://deezerdevs-deezer.p.rapidapi.com/search?q=${this.artista}`,
+    //     {
+    //       headers: {
+    //         "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    //         "x-rapidapi-key":
+    //           "0582757109msh175aa52bbe104a7p104799jsn7e282369ac23"
+    //       }
+    //     }
+    //   )
+    //   .then(response => {
+    //     this.items = response.data.data;
+    //   });
   },
   data() {
     return {
-      items: []
+      items: [],
+      artista: "Eminem",
+      cantidad: 5
     };
+  },
+  methods: {
+    filterData: function() {
+      axios
+        .get(
+          `https://deezerdevs-deezer.p.rapidapi.com/search?q=${this.artista}`,
+          {
+            headers: {
+              "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+              "x-rapidapi-key":
+                "0582757109msh175aa52bbe104a7p104799jsn7e282369ac23"
+            }
+          }
+        )
+        .then(response => {
+          this.items = response.data.data;
+        });
+    },
+    mostrarMas: function() {
+      this.cantidad += 5;
+    }
   }
+};
+
+String.prototype.toHHMMSS = function() {
+  var sec_num = parseInt(this, 10); // don't forget the second param
+  var hours = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - hours * 3600) / 60);
+  var seconds = sec_num - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + ":" + minutes + ":" + seconds;
 };
 </script>
 
@@ -53,7 +107,7 @@ ul {
   list-style: none;
   padding: 0;
   box-sizing: border-box;
-  width: 60%;
+  width: 40%;
   margin: 0 auto;
 }
 li {
@@ -61,12 +115,31 @@ li {
   width: 100%;
   box-sizing: border-box;
   border: 1px solid #ccc;
-  padding: 10px;
+  padding: 20px;
   border-radius: 2px;
   box-shadow: 0px 2px 10px #00000036;
   display: flex;
 }
 a {
   color: #42b983;
+}
+
+.descrip {
+  text-align: left;
+  font-size: 14px;
+}
+
+.descrip span {
+  font-size: 12px;
+}
+
+.busqueda {
+  margin: 10px auto;
+}
+
+input,
+button {
+  padding: 5px 12px;
+  margin-left: 10px;
 }
 </style>
